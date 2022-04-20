3fd0455b18341a3965c7ce2fa7668a6f1a384431
kvpatel333 <kvpatel333@yahoo.com>
2022-04-20 18:55:33 +0530

    Modified content

--- ea-sample-webpage.ps1
+++ ea-sample-webpage.ps1
@@ -1,2 +1,3 @@
 Add-WindowsFeature Web-Server
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "This is East Asia Webpage"
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername) 
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value  "\nThis is East Asia Webpage"
