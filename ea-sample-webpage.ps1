180489487d5b244e80371b79a49c7d310dabd3c0
kvpatel333 <kvpatel333@yahoo.com>
2022-04-20 18:50:42 +0530

    Modified content

--- ea-sample-webpage.ps1
+++ ea-sample-webpage.ps1
@@ -1,3 +1,2 @@
 Add-WindowsFeature Web-Server
-New-Item -ItemType file -Path "C:\inetpub\wwwroot\Default.htm"
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value 'This is East Asia Webpage'
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value "This is East Asia Webpage"
