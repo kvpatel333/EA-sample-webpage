904f8f107ed2e1201027bc44da58e433ff98708e
kvpatel333 <kvpatel333@yahoo.com>
2022-04-21 14:10:17 +0530

    EAST ASIA Web Page

--- ea-sample-webpage.ps1
+++ ea-sample-webpage.ps1
@@ -1,2 +1,2 @@
 Add-WindowsFeature Web-Server
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value 'This is EAST INDIA Web Page'
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value 'This is EAST ASIA Web Page'
